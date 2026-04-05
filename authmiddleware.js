6d591723a3578d38e4040247844ef9dac1f27d7f
Bibek-M <bibek7020u@gmail.com>
2026-04-05 22:25:35 +0530

    AuthMiddleware Fix

--- authmiddleware.js
+++ authmiddleware.js
@@ -1,24 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 function authMiddleware(req, res, next) {
-  const token = req.headers.token;
+  const token = req.headers.authorization;
+
   if (!token) {
-    res.status(403).json({
-      message: "Wrong Token",
+    return res.status(403).json({
+      message: "Token missing",
     });
-    return;
   }
-  const decoded = jwt.verify(token, "secret123");
-  const userid = decoded.userid;
-  if (decoded) {
-    req.userid = parseInt(userid);
+
+  try {
+    const decoded = jwt.verify(token, "secret123");
+
+    req.userid = decoded.userid;
+
     next();
-  } else {
-    res.status(403).json({
-      message: "Wrong secret",
+  } catch (err) {
+    return res.status(403).json({
+      message: "Invalid token",
     });
   }
 }
+
 module.exports = {
-  authMiddleware: authMiddleware,
+  authMiddleware,
 };
